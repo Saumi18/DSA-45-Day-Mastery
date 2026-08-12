3f5185427bb9575d29c1ff825780adc2a9e7c5e2
Saumitra Kulkarni <147703962+Saumi18@users.noreply.github.com>
2026-08-12 14:51:15 +0530

    Solved 03_buy_and_sell_stock.cpp

--- Day_01/03_buy_and_sell_stock.cpp
+++ Day_01/03_buy_and_sell_stock.cpp
@@ -2,15 +2,42 @@
 // Problem: Buy & Sell Stock
 //
 // My notes:
-// Pattern: 
-// Time: 
-// Space: 
+// Pattern: Two pointer or one pass (Greedy)
+// Time: O(n)
+// Space: O(1)
 
+// This is two pointer approach
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int max_profit = 0;
+        int l=0, r=1;
+        while(r<prices.size()){
+            if(prices[l]<prices[r]){
+                int profit = prices[r]-prices[l];
+                max_profit = max(profit,max_profit);
+            }
+            else l = r;
+            r++;
+        }
+        return max_profit;
+    }
+};
 
-#include <bits/stdc++.h>
-using namespace std;
+// One Pass Greedy approach we maintin the minimum buy price and sell price is the iterator
 
 class Solution {
 public:
-    // TODO: Implement Buy & Sell Stock
+    int maxProfit(vector<int>& prices) {
+        int n  = prices.size();
+        int minBuy = prices[0];
+        int maxProfit = 0;
+        for(int sell=0;sell<n;sell++){
+            int profit = prices[sell] - minBuy;
+            minBuy = min(minBuy,prices[sell]);
+            maxProfit = max(maxProfit,profit);
+        }
+        return maxProfit;
+    }
 };
+
