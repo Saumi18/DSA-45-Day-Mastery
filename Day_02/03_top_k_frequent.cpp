898d0f3263003443f78c28ab46a581d6bd8aa390
Saumitra Kulkarni <147703962+Saumi18@users.noreply.github.com>
2026-08-12 23:10:38 +0530

    solved 03_top_k_frequent.cpp

--- Day_02/03_top_k_frequent.cpp
+++ Day_02/03_top_k_frequent.cpp
@@ -2,15 +2,36 @@
 // Problem: Top K Frequent
 //
 // My notes:
-// Pattern: 
-// Time: 
-// Space: 
+// Pattern: HashMap to count frequency + Bucket Sort.
+// bucket[freq] stores all numbers appearing 'freq' times.
+// vector<vector<int>> is used because one frequency can have multiple numbers.
+// Traverse buckets from highest frequency to lowest and take k elements.
+// Time: O(n)
+// Space: O(n)
 
-
-#include <bits/stdc++.h>
-using namespace std;
 
 class Solution {
 public:
-    // TODO: Implement Top K Frequent
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        unordered_map<int,int> mp;
+        vector<int> result;
+        for(int i=0;i<nums.size();i++){
+            mp[nums[i]]++;
+        }
+        vector<vector<int>> bucket(nums.size()+1);
+        for (auto& p : mp) {
+            int number = p.first;
+            int frequency = p.second;
+
+            bucket[frequency].push_back(number);
+        }
+        for (int freq = nums.size(); freq >= 1; freq--) {
+            for (int i = 0; i < bucket[freq].size(); i++) {
+                result.push_back(bucket[freq][i]);
+                if (result.size() == k) return result;
+                }
+            }
+            return result;
+        }
 };
+
