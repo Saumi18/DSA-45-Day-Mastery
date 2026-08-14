0bceba56448cd3c429622ebde91718bcec20400d
Saumitra Kulkarni <147703962+Saumi18@users.noreply.github.com>
2026-08-14 19:41:09 +0530

    Solved 02_two_sum_ii.cpp

--- Day_04/02_two_sum_ii.cpp
+++ Day_04/02_two_sum_ii.cpp
@@ -1,16 +1,29 @@
 // Day 04 — Two Pointers
-// Problem: Two Sum II
+// Problem: Two Sum II : Sorted Array
 //
 // My notes:
-// Pattern: 
-// Time: 
-// Space: 
+// Pattern: Two pointers on sorted array as hash map will spend O(n) S.C.
+// Time: O(n)
+// Space: O(1)
 
-
-#include <bits/stdc++.h>
-using namespace std;
 
 class Solution {
 public:
-    // TODO: Implement Two Sum II
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        vector<int> result;
+        int l=0;
+        int r = numbers.size()-1;
+        while(l<r){
+            if(numbers[l]+numbers[r] == target){
+                result.push_back(l+1);
+                result.push_back(r+1);
+                break;
+            }
+            else if(numbers[l]+numbers[r] < target){
+                l++;
+            }
+            else r--;
+        }
+        return result;
+    }
 };
