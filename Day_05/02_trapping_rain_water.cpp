d9bbb9cb04f7350080209cd1fa558b182e3668ec
Saumitra Kulkarni <147703962+Saumi18@users.noreply.github.com>
2026-08-15 20:57:40 +0530

    Solved 02_trapping_rain_water.cpp

--- Day_05/02_trapping_rain_water.cpp
+++ Day_05/02_trapping_rain_water.cpp
@@ -2,15 +2,35 @@
 // Problem: Trapping Rain Water
 //
 // My notes:
-// Pattern: 
-// Time: 
-// Space: 
-
-
-#include <bits/stdc++.h>
-using namespace std;
+// Pattern: Two Pointers + track maximum height from both sides.
+// Move the shorter side because it determines the limiting water level.
+// Time: O(n)
+// Space: O(1)
 
 class Solution {
 public:
-    // TODO: Implement Trapping Rain Water
+    int trap(vector<int>& height) {
+        int res = 0;
+        int l = 0;
+        int r = height.size() - 1;
+        int maxLeft = height[l];
+        int maxRight = height[r];
+
+        while(l < r) {
+            if(height[l] < height[r]) {
+                // Right side is high enough, so left max limits water
+                maxLeft = max(maxLeft, height[l]);
+                res += maxLeft - height[l];
+                l++;
+            }
+            else {
+                // Left side is high enough, so right max limits water
+                maxRight = max(maxRight, height[r]);
+                res += maxRight - height[r];
+                r--;
+            }
+        }
+
+        return res;
+    }
 };
