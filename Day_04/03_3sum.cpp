9b7e9c04ba7ca6dc337d3a8cc13df126c17f3a61
Saumitra Kulkarni <147703962+Saumi18@users.noreply.github.com>
2026-08-15 11:38:06 +0530

    Solved03_3sum.cpp

--- Day_04/03_3sum.cpp
+++ Day_04/03_3sum.cpp
@@ -2,15 +2,37 @@
 // Problem: 3Sum
 //
 // My notes:
-// Pattern: 
-// Time: 
-// Space: 
+// Pattern: Sort + Fix one element + Two Pointers; skip duplicates for unique triplets.
+// Time: O(n²)
+// Space: O(1) extra space (excluding output)
 
-
-#include <bits/stdc++.h>
-using namespace std;
 
 class Solution {
 public:
-    // TODO: Implement 3Sum
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int>> res;
+        sort(nums.begin(),nums.end());
+        for(int i = 0;i<nums.size();i++){
+            if(i>0 && nums[i]==nums[i-1]) continue; // THis is to avoid duplicates
+            int target = -nums[i];
+            int l =i+1;
+            int r = nums.size()-1;
+            while(l<r){
+                if(nums[l]+nums[r] == target){
+                    res.push_back({nums[i],nums[l],nums[r]});
+                    // Skip duplicate left/right values
+                    while(l < r && nums[l] == nums[l + 1])
+                        l++;
+
+                    while(l < r && nums[r] == nums[r - 1])
+                        r--;
+                    l++;
+                    r--;
+                }
+                else if(nums[l]+nums[r] > target) r--;
+                else l++;
+            }
+        }
+        return res;
+    }
 };
