7bf277c6508f0d5a287b4edc89a8933265b5cfd4
Saumitra Kulkarni <147703962+Saumi18@users.noreply.github.com>
2026-08-19 15:49:20 +0530

    Solved03_merge_intervals.cpp

--- Day_09/03_merge_intervals.cpp
+++ Day_09/03_merge_intervals.cpp
@@ -2,15 +2,30 @@
 // Problem: Merge Intervals
 //
 // My notes:
-// Pattern: 
-// Time: 
-// Space: 
+// Pattern: Sort + Merge Intervals
+// Idea: Sort by start time, then compare with last interval in ans.
+// If overlap → merge using max ending time.
+// If no overlap → push current interval.
+//
+// Time: O(n log n) → sorting + O(n) merge
+// Space: O(n) → output
 
-
-#include <bits/stdc++.h>
-using namespace std;
 
 class Solution {
 public:
-    // TODO: Implement Merge Intervals
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        vector<vector<int>> ans;
+        sort(intervals.begin(),intervals.end());
+        for(int i=0;i<intervals.size();i++){
+            if(ans.empty() || ans.back()[1]<intervals[i][0]){
+                ans.push_back(intervals[i]);
+            }
+            else{
+                ans.back()[0] = min(ans.back()[0],intervals[i][0]);
+                ans.back()[1] = max(ans.back()[1],intervals[i][1]);
+            }
+        }
+        return ans;
+    }
 };
+
