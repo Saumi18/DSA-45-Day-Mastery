b41ddcf6061b87fee3ef181a019ba89128ff8e56
Saumitra Kulkarni <147703962+Saumi18@users.noreply.github.com>
2026-08-21 16:26:06 +0530

    Solved 01_valid_parentheses.cpp

--- Day_11/01_valid_parentheses.cpp
+++ Day_11/01_valid_parentheses.cpp
@@ -2,15 +2,39 @@
 // Problem: Valid Parentheses
 //
 // My notes:
-// Pattern: 
-// Time: 
-// Space: 
+// Pattern: Stack + Matching Pairs
+//
+// Intuition:
+// Opening bracket → push into stack
+// Closing bracket → check top, if matching → pop
+// Mismatch OR empty stack → false
+// At the end, stack must be empty
+//
+// Important:
+// Never call st.top() without checking !st.empty()
+// Otherwise → runtime error
+//
+// Time: O(n)
+// Space: O(n)
 
-
-#include <bits/stdc++.h>
-using namespace std;
 
 class Solution {
 public:
-    // TODO: Implement Valid Parentheses
+    bool isValid(string s) {
+        stack<char> st;
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='(' || s[i]=='{' || s[i]=='['){
+                st.push(s[i]);
+            }
+            else if(!st.empty() && ((s[i]=='}' && st.top()=='{')||(s[i]==')' && st.top()=='(')||(s[i]==']' && st.top()=='['))){
+                st.pop();
+            }
+            else {
+                return false;
+            }
+        }
+        if(st.empty()) return true;
+        else return false;
+    }
 };
+
