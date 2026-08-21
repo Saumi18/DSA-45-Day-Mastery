aa5918021de7612c88f166ba87b04ed9b749f24e
Saumitra Kulkarni <147703962+Saumi18@users.noreply.github.com>
2026-08-21 19:20:23 +0530

    Solved 03_evaluate_reverse_polish_notation.cpp

--- Day_11/03_evaluate_reverse_polish_notation.cpp
+++ Day_11/03_evaluate_reverse_polish_notation.cpp
@@ -2,15 +2,52 @@
 // Problem: Evaluate Reverse Polish Notation
 //
 // My notes:
-// Pattern: 
-// Time: 
-// Space: 
+// Pattern: Stack + Postfix Evaluation
+// Number → push | Operator → pop 2, calculate, push result
+// For - and /: temp2 op temp1
+// stoi() converts string → int
+// Time: O(n)
+// Space: O(n)
 
-
-#include <bits/stdc++.h>
-using namespace std;
 
 class Solution {
 public:
-    // TODO: Implement Evaluate Reverse Polish Notation
+    int evalRPN(vector<string>& tokens) {
+        stack<int> st;
+        for(int i=0;i<tokens.size();i++){
+            if(tokens[i]!="*" && tokens[i]!="/" && tokens[i]!="+" && tokens[i]!="-"){
+                st.push(stoi(tokens[i]));
+            }
+            if(tokens[i]=="*" && st.size()>=2){
+                int temp1 = st.top();
+                st.pop();
+                int temp2 = st.top();
+                st.pop();
+                st.push(temp1*temp2);
+            }
+            if(tokens[i]=="+" && st.size()>=2){
+                int temp1 = st.top();
+                st.pop();
+                int temp2 = st.top();
+                st.pop();
+                st.push(temp1+temp2);
+            }
+            if(tokens[i]=="-" && st.size()>=2){
+                int temp1 = st.top();
+                st.pop();
+                int temp2 = st.top();
+                st.pop();
+                st.push(temp2-temp1);
+            }
+            if(tokens[i]=="/" && st.size()>=2){
+                int temp1 = st.top();
+                st.pop();
+                int temp2 = st.top();
+                st.pop();
+                if(temp1 != 0) st.push(temp2/temp1);
+            }
+        }
+        return st.top();
+    }
 };
+
