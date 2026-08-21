7ca2068376f6942c9a8ab535a4fba5db19455c6b
Saumitra Kulkarni <147703962+Saumi18@users.noreply.github.com>
2026-08-21 17:47:51 +0530

    SOlved 02_min_stack.cpp

--- Day_11/02_min_stack.cpp
+++ Day_11/02_min_stack.cpp
@@ -2,15 +2,46 @@
 // Problem: Min Stack
 //
 // My notes:
-// Pattern: 
-// Time: 
-// Space: 
+// Pattern: Stack + Auxiliary Stack
+// Idea: Use minSt to keep track of minimum elements
+// getMin(): minSt.top() gives minimum in O(1)
+// Push: push into minSt if val <= current minimum
+// Pop: if st.top() == minSt.top(), pop from both
+// Time: O(1) for push, pop, top, getMin
+// Space: O(n)
 
 
-#include <bits/stdc++.h>
-using namespace std;
-
-class Solution {
+class MinStack {
 public:
-    // TODO: Implement Min Stack
+    stack<int> st;
+    stack<int> minSt;
+    MinStack() {
+    }
+    
+    void push(int val) {
+        st.push(val);
+        if(minSt.empty()) minSt.push(val);
+        else if(minSt.top()>=val){
+            minSt.push(val);
+        }
+        else return;
+    }
+    
+    void pop() {
+        if(minSt.top()==st.top()){
+            st.pop();
+            minSt.pop();
+        }
+        else st.pop();
+        return;
+    }
+    
+    int top() {
+        return st.top();
+    }
+    
+    int getMin() {
+        return minSt.top();
+    }
 };
+
