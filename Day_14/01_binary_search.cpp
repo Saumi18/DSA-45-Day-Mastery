cbd558917840c3e7019e9403f1ffc956497e7492
Saumitra Kulkarni <147703962+Saumi18@users.noreply.github.com>
2026-08-24 14:13:46 +0530

    Solved 01_binary_search.cpp

--- Day_14/01_binary_search.cpp
+++ Day_14/01_binary_search.cpp
@@ -2,15 +2,29 @@
 // Problem: Binary Search
 //
 // My notes:
-// Pattern: 
-// Time: 
-// Space: 
-
-
-#include <bits/stdc++.h>
-using namespace std;
+// Pattern: Binary Search / Divide and Conquer
+// Logic: Check the middle element and eliminate half of the search space
+// If nums[mid] > target, search left half
+// If nums[mid] < target, search right half
+// Use mid = l + (r-l)/2 to avoid integer overflow
+// Time: O(log n)
+// Space: O(1)
 
 class Solution {
 public:
-    // TODO: Implement Binary Search
+    int search(vector<int>& nums, int target) {
+        int l = 0, r = nums.size() - 1;
+
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+            if (nums[mid] == target) return mid;
+            if (nums[mid] > target){
+                r = mid - 1;
+            }
+            else{
+                l = mid + 1;
+            }
+        }
+        return -1;
+    }
 };
