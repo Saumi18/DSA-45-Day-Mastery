bda0e09bbf14001c47875b66880f0e962a9a6587
Saumitra Kulkarni <147703962+Saumi18@users.noreply.github.com>
2026-08-24 15:27:33 +0530

    Solved 02_search_a_2d_matrix.cpp

--- Day_14/02_search_a_2d_matrix.cpp
+++ Day_14/02_search_a_2d_matrix.cpp
@@ -2,15 +2,33 @@
 // Problem: Search a 2D Matrix
 //
 // My notes:
-// Pattern: 
-// Time: 
-// Space: 
-
-
-#include <bits/stdc++.h>
-using namespace std;
+// Pattern: Binary Search on 2D Matrix
+// Logic: Treat the matrix as a sorted 1D array
+// Convert 1D index to 2D using:
+// row = mid / n
+// col = mid % n
+// If matrix[row][col] < target, search right half
+// If matrix[row][col] > target, search left half
+// Time: O(log(m*n))
+// Space: O(1)
 
 class Solution {
 public:
-    // TODO: Implement Search a 2D Matrix
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+        int l = 0, r = m * n - 1;
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+            // row = mid / n;
+            // col = mid % n;
+            if (matrix[mid / n][mid % n] == target)
+                return true;
+            if (matrix[mid / n][mid % n] < target)
+                l = mid + 1;
+            else
+                r = mid - 1;
+        }
+        return false;
+    }
 };
