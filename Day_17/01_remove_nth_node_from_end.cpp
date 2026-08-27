83d1b07c64ff796981adf0fbf32d0ec432c38244
Saumitra Kulkarni <147703962+Saumi18@users.noreply.github.com>
2026-08-27 11:34:09 +0530

    Update 01_remove_nth_node_from_end.cpp

--- Day_17/01_remove_nth_node_from_end.cpp
+++ Day_17/01_remove_nth_node_from_end.cpp
@@ -2,15 +2,38 @@
 // Problem: Remove Nth Node From End
 //
 // My notes:
-// Pattern: 
-// Time: 
-// Space: 
+// Pattern: Find length → Find node before target → Skip target
+// Time: O(n)
+// Space: O(1)
 
-
-#include <bits/stdc++.h>
-using namespace std;
 
 class Solution {
 public:
-    // TODO: Implement Remove Nth Node From End
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        // Find the length of the linked list
+        ListNode* temp = head;
+        int m = 0;
+
+        while (temp != NULL) {
+            m++;
+            temp = temp->next;
+        }
+        // If nth node from end is the head
+        if (m == n)
+            return head->next;
+        // Move to the node just before the node to remove
+        int cnt = 1;
+        temp = head;
+        while (cnt != m - n) {
+            temp = temp->next;
+            cnt++;
+        }
+        // t1 = node to remove
+        // t2 = node after the node to remove
+        ListNode* t1 = temp->next;
+        ListNode* t2 = temp->next->next;
+        // Skip t1
+        temp->next = t2;
+        return head;
+    }
 };
