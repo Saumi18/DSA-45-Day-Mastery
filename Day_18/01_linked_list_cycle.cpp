b7e31e00c6824b4ed1b3b37d84ed39b0d7e323cf
Saumitra Kulkarni <147703962+Saumi18@users.noreply.github.com>
2026-08-28 17:44:03 +0530

    Solved 01_linked_list_cycle.cpp

--- Day_18/01_linked_list_cycle.cpp
+++ Day_18/01_linked_list_cycle.cpp
@@ -2,15 +2,27 @@
 // Problem: Linked List Cycle
 //
 // My notes:
-// Pattern: 
-// Time: 
-// Space: 
-
-
-#include <bits/stdc++.h>
-using namespace std;
+// Pattern: Hash Map / Visited Nodes
+// Time: O(n)
+// Space: O(n)
+//
+// Idea:
+// Store every node's address in a hash map.
+// If we encounter the same node again, a cycle exists.
+// If temp becomes NULL, there is no cycle.
 
 class Solution {
 public:
-    // TODO: Implement Linked List Cycle
+    bool hasCycle(ListNode* head) {
+        unordered_map<ListNode*, int> mp;
+        ListNode* temp = head;
+
+        while (temp != NULL) {
+            mp[temp]++;
+            if (mp[temp] > 1)
+                return true;
+            temp = temp->next;
+        }
+        return false;
+    }
 };
