b187fd612355d160a5782d7189f3496919146a35
Saumitra Kulkarni <147703962+Saumi18@users.noreply.github.com>
2026-08-28 23:49:43 +0530

    Update 01_linked_list_cycle.cpp

--- Day_18/01_linked_list_cycle.cpp
+++ Day_18/01_linked_list_cycle.cpp
@@ -26,3 +26,19 @@ public:
         return false;
     }
 };
+
+// Approach 2: Floyd cycle use slow and fast pointers
+class Solution {
+public:
+    bool hasCycle(ListNode* head) {
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while(fast!=NULL && fast->next!=NULL){
+            slow = slow->next;
+            fast = fast->next->next;
+            if(slow == fast) return true;
+        }
+        return false;
+    }
+};
+
