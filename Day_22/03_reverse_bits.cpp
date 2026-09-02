7245b7364b956b325995b812bcba6426a707b99e
Saumitra Kulkarni <147703962+Saumi18@users.noreply.github.com>
2026-09-02 19:00:28 +0530

    Update 03_reverse_bits.cpp

--- Day_22/03_reverse_bits.cpp
+++ Day_22/03_reverse_bits.cpp
@@ -2,15 +2,24 @@
 // Problem: Reverse Bits
 //
 // My notes:
-// Pattern: 
-// Time: 
-// Space: 
+// Pattern: Bit Extraction + Bit Shifting
+// Idea: Extract the i-th bit using (n >> i) & 1, then place it at the
+// reversed position (31 - i) in the result.
+//
+// Time: O(32) = O(1)
+// Space: O(1)
 
-
-#include <bits/stdc++.h>
-using namespace std;
 
 class Solution {
 public:
-    // TODO: Implement Reverse Bits
+    uint32_t reverseBits(uint32_t n) {
+        uint32_t res =0;
+        for(int i=0;i<32;i++){
+            uint32_t bit = (n>>i)&1;
+            res += (bit<<(31-i));
+        }
+        return res;
+    }
 };
+
+
