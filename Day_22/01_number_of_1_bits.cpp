edf37bf266d22846be19ad4bda030db33005e744
Saumitra Kulkarni <147703962+Saumi18@users.noreply.github.com>
2026-09-02 12:15:36 +0530

    Update 01_number_of_1_bits.cpp

--- Day_22/01_number_of_1_bits.cpp
+++ Day_22/01_number_of_1_bits.cpp
@@ -2,15 +2,23 @@
 // Problem: Number of 1 Bits
 //
 // My notes:
-// Pattern: 
-// Time: 
-// Space: 
+// Pattern: Bit Manipulation + Right Shift
+// Idea: Check the last bit using (n & 1) also can be checked using (n%2). If it is 1, add it to the result.
+// Right shift n by 1 to check the next bit. Continue until n becomes 0.
+//
+// Time: O(32) = O(1)
+// Space: O(1)
 
-
-#include <bits/stdc++.h>
-using namespace std;
 
 class Solution {
 public:
-    // TODO: Implement Number of 1 Bits
+    int hammingWeight(uint32_t n) {
+        int res = 0;
+        while (n) {
+            res += n & 1;
+            // instead of n&1 we can also do n % 2 -> both give same ans
+            n = n >> 1;
+        }
+        return res;
+    }
 };
